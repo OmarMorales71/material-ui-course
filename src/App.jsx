7b392f3fc3270ec327bbb99ea1bff013c6ff2a8f
mancubus <mancubus@sourcedevil.com>
2020-08-07 12:42:13 -0500

    Using Grid pt1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MakeTheme from "./classes/MakeTheme";
 import DarkMode from "./classes/DarkMode";
+import GridExample from "./classes/GridExample";
 
 
 
 function App() {
   
   return (
-    <DarkMode />
+    <GridExample />
   );
 }
 
---
+++ src/classes/GridExample.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grid, Divider } from "@material-ui/core";
+
+const GridExample = () => {
+  return (
+    <div>
+      <Grid container spacing={2} direction="column">
+        <Grid item xs={12} container>
+          <Grid item xs={2}>
+            Welcome Omar
+          </Grid>
+          <Grid item xs={8} />
+
+          <Grid item xs={2}>
+            Logout
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+            <Divider />
+        </Grid>
+        <Grid item xs={12} container spacing={2} >
+            <Grid item xs={6} >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi at quia ducimus, id nesciunt quo eveniet eaque reprehenderit. Optio perferendis id repellat! Omnis non dolorum beatae ex laboriosam cum perspiciatis?
+            </Grid>
+            <Grid item xs={6}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi at quia ducimus, id nesciunt quo eveniet eaque reprehenderit. Optio perferendis id repellat! Omnis non dolorum beatae ex laboriosam cum perspiciatis?
+            </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default GridExample;
